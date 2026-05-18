67d0a81e1489cb26433bc0976f102658624316de
abrilmayleth <amstchocolate@gmail.com>
2026-05-18 02:03:45 +0200

    guardado final

--- app/src/main/java/com/example/herafit/AddRutinaActivity.kt
+++ app/src/main/java/com/example/herafit/AddRutinaActivity.kt
@@ -1,16 +1,15 @@
 package com.example.herafit
 
-import android.content.Intent
+import com.google.android.material.textfield.TextInputEditText
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 
-
 class AddRutinaActivity : AppCompatActivity() {
+
     private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,31 +18,30 @@ class AddRutinaActivity : AppCompatActivity() {
 
         db = FirebaseFirestore.getInstance()
 
+        val nombre = findViewById<TextInputEditText>(R.id.nombre)
+        val descripcion = findViewById<TextInputEditText>(R.id.descripcion)
 
-        val nombre = findViewById<EditText>(R.id.nombre)
-        val descripcion = findViewById<EditText>(R.id.descripcion)
         val backBtn = findViewById<ImageView>(R.id.backBtn)
+        val saveBtn = findViewById<Button>(R.id.saveBtn)
+        val cancelBtn = findViewById<Button>(R.id.cancelBtn)
 
-        // flecha de volver
+
         backBtn.setOnClickListener {
             finish()
         }
 
-        // boton de guardar,
-        findViewById<Button>(R.id.saveBtn).setOnClickListener {
-            val rutina = Rutina(
-                nombre = nombre.text.toString(),
-                descripcion = descripcion.text.toString()
+
+        cancelBtn.setOnClickListener {
+            finish()
+        }
+
+        // guardar
+        saveBtn.setOnClickListener {
+
+            val rutina = hashMapOf(
+                "nombre" to nombre.text.toString(),
+                "descripcion" to descripcion.text.toString()
             )
-
-            val cancelBtn = findViewById<Button>(R.id.cancelBtn)
-            cancelBtn.setOnClickListener {
-                // Vuelve al perfil
-                startActivity(Intent(this, PerfilActivity::class.java))
-                finish()
-            }
-
-            // mensajes de guardado, cancelado
 
             db.collection("rutinas")
                 .add(rutina)
@@ -55,7 +53,7 @@ class AddRutinaActivity : AppCompatActivity() {
                         Toast.LENGTH_SHORT
                     ).show()
 
-                    finish() // vuelve a RutinasActivity
+                    finish()
                 }
                 .addOnFailureListener { e ->
 
